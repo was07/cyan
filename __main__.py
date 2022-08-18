23218b22c0c81cd0db16dfaf02edbcc9df6ef07f
Was <93242673+was07@users.noreply.github.com>
2022-08-18 18:51:54 +0600

    Update __main__.py
    Add more options, update shell

--- __main__.py
+++ __main__.py
@@ -1,4 +1,3 @@
-import time
 import sys
 
 from src.interpreter import run
@@ -9,21 +8,19 @@ DEBUG = 0
 
 
 def shell():
-    print(f'Cyan {__version__}')
-    try:
+    print(f'Cyan {__version__} shell on {sys.platform}')
+    while True:
+        try:
+            text = input('>>> ')
+        except KeyboardInterrupt:
+            print('\nExiting...'); sys.exit(0)
+        
+        result, error = run('<stdin>', text, debug_mode=DEBUG)
 
-        while True:
-            text = input('&> ')
-            result, error = run('<stdin>', text, debug_mode=DEBUG)
-
-            if error:
-                print(error)
-            else:
-                print(result)
-
-    except KeyboardInterrupt:
-        print('Exiting...')
-        time.sleep(1)
+        if error:
+            print(error)
+        else:
+            print(result)
 
 
 def run_file(filename):
@@ -40,10 +37,26 @@ def run_file(filename):
 
 def main():
     argv = sys.argv[1:]
-    if len(argv) and argv[0] == 'd':
+    if argv[0] in ('-d', '--debug'):
         DEBUG = True
+        argv = argv[1:]
+    if argv[0] == ('--version'):
+        print(__version__)
+        sys.exit(0)
+    if argv[0] == '--help':
+        print(f'Usage: {sys.argv[0]} [options] [file]')
+        print(f'Options:')
+        print(f'  -d, --debug    Enable debug mode')
+        print(f'  --version      Show version')
+        print(f'  --help         Show this help')
+        print(f'  file           Run the file')
+        sys.exit(0)
+    if len(argv) == 1:
+        run_file(argv[0])
+        sys.exit(0)
+
     shell()
-    
+
     
 if __name__ == '__main__':
     main()
